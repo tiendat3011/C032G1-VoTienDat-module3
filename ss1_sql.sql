36c2439638b4d0b5bc81e4a37d5f1ed49e321ca4
tiendat3011 <tiendat30111999@gmail.com>
2022-06-13 13:58:09 +0700

    ok

--- ss1_sql.sql
+++ ss1_sql.sql
@@ -1,9 +1,7 @@
 create database `my_database`;
-CREATE TABLE `student`(
+CREATE TABLE `class`(
 id INT NOT NULL PRIMARY KEY,
-`name` VARCHAR(45),
-age INT NULL,
-`country` VARCHAR(45)NULL);
+`name` VARCHAR(45));
 CREATE TABLE `teacher`(
 id INT NOT NULL PRIMARY KEY,
 `name` VARCHAR(45),
