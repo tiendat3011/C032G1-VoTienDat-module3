7ad94fc0e1cda3352f62078e8e0504169ffeb1ac
tiendat3011 <tiendat30111999@gmail.com>
2022-06-13 10:54:37 +0700

    ok

---
+++ ss1_sql.sql
@@ -0,0 +1,11 @@
+create database `my_database`;
+CREATE TABLE `student`(
+id INT NOT NULL PRIMARY KEY,
+`name` VARCHAR(45),
+age INT NULL,
+`country` VARCHAR(45)NULL);
+CREATE TABLE `teacher`(
+id INT NOT NULL PRIMARY KEY,
+`name` VARCHAR(45),
+age INT NULL,
+`country` VARCHAR(45)NULL);
