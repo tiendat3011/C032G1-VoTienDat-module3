c29a7daab0f1668a8223d1bd1695d1a963aafda8
tiendat3011 <tiendat30111999@gmail.com>
2022-06-16 21:42:25 +0700

    ok

--- ss4_common_functions_sql/exercise/PracticeCommonFunctionsSQL.sql
+++ ss4_common_functions_sql/exercise/PracticeCommonFunctionsSQL.sql
@@ -1,16 +1,11 @@
 use quan_ly_sinh_vien;
 
-SELECT 
-    *, MAX(credit)
-FROM
-    `subject`;
+SELECT *, MAX(credit)
+FROM`subject`;
 
-SELECT 
-    *, MAX(mark)
-FROM
-    mark
-        JOIN
-    `subject` ON `subject`.sub_id = mark.sub_id;
+SELECT *, MAX(mark)
+FROM mark
+JOIN`subject` ON `subject`.sub_id = mark.sub_id;
 
 SELECT *, AVG(mark.mark)
 FROM student 
