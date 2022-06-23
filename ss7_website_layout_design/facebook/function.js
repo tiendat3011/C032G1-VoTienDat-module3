086296c0eabd3283c63eec84494185c4d23fa221
tiendat3011 <tiendat30111999@gmail.com>
2022-06-23 19:37:22 +0700

    ok

---
+++ ss7_website_layout_design/facebook/function.js
@@ -0,0 +1,16 @@
+var userSettings = document.querySelector(".user-settings");
+var darkBtn = document.getElementById("dark-button");
+var LoadMoreBackground =document.querySelector(".btn-LoadMore");
+function UserSettingToggle(){
+    userSettings.classList.toggle("user-setting-showup-toggle");
+}
+
+
+function darkModeON(){
+    darkBtn.classList.toggle("dark-mode-on");
+   document.body.classList.toggle("dark-theme");
+};
+
+function LoadMoreToggle(){
+    LoadMoreBackground.classList.toggle("loadMoreToggle");
+};
